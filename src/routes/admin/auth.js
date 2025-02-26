594d804e1493c55fe8f7dcb040c95ff0b38e9fe0
Omar khaled <96238185+omarzo0@users.noreply.github.com>
2025-02-27 01:35:29 +0200

    add validation but i have issue in localization

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { adminLogin } = require("../../controller/admin/auth");
+const { validateBody } = require("../../middleware/validation");
+const { adminLoginSchema } = require("../../validation/admin/auth");
 
 const router = express.Router();
 
-router.post("/login", adminLogin);
+router.post("/login", validateBody(adminLoginSchema), adminLogin);
 
 module.exports = router;
