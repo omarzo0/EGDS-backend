9bd0eaa54151bca884434f886b2971692e86c4b2
omar_khaled_72 <omarkhaled202080@gmail.com>
2025-03-18 03:52:09 +0200

    remove logs and update database

--- src/database/models/services.js
+++ src/database/models/services.js
@@ -3,12 +3,16 @@ const mongoose = require("mongoose");
 const serviceSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    Description: {type:string, required:true},
     department_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Department",
       required: true,
-      index: true, // Added for performance
+      index: true, 
     },
+    fees: { type: Number, required: true }, 
+    points: { type: Number, required: true }, 
+    processing_time: { type: String, required: true }, 
   },
   { timestamps: true }
 );
